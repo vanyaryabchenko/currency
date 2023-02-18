1e73d9b43ceb2b86d397bcdb39c5cbda1ffeb922
vanya <vanya.ryabchenko@gmail.com>
2023-02-18 13:19:38 +0200

    add model ContactUs

--- app/currency/models.py
+++ app/currency/models.py
@@ -9,3 +9,9 @@ class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sell = models.DecimalField(max_digits=6, decimal_places=2)
     source = models.CharField(max_length=25)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField()
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from currency.views import list_rates
+from currency.views import list_rates, list_contactus
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rate/list/', list_rates)
+    path('rate/list/', list_rates),
+    path('contactus/list', list_contactus)
 ]
