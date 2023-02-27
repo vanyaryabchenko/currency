7c6fe1e8c9727c90ff51fc0f22d1a5abf8184603
vanya <vanya.ryabchenko@gmail.com>
2023-02-27 23:07:02 +0200

    hw8

--- app/currency/models.py
+++ app/currency/models.py
@@ -15,3 +15,8 @@ class ContactUs(models.Model):
     email_from = models.EmailField()
     subject = models.CharField(max_length=50)
     message = models.TextField()
+
+
+class Source(models.Model):
+    source_url = models.URLField(max_length=255)
+    name = models.CharField(max_length=64)
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from currency.models import Rate, ContactUs
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from currency.forms import SourceForm
+from currency.models import Rate, ContactUs, Source
 
 
 # Create your views here.
@@ -38,3 +40,60 @@ def template_contactus(request):
         'contactus': contactus
     }
     return render(request, 'contactus_table.html', context)
+
+
+def template_source(request):
+    source = Source.objects.all()
+    context = {
+        'source': source
+    }
+    return render(request, 'source_table.html', context)
+
+
+def source_create(request):
+    if request.method == 'POST':
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/template/source')
+    elif request.method == 'GET':
+        form = SourceForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'source_create.html', context)
+
+
+def source_update(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        form = SourceForm(request.POST, instance=source)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/template/source')
+    elif request.method == 'GET':
+        form = SourceForm(instance=source)
+    context = {
+        'form': form
+    }
+    return render(request, 'source_update.html', context)
+
+
+def source_delete(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        source.delete()
+        return HttpResponseRedirect('/template/source')
+    elif request.method == 'GET':
+        context = {
+            'source': source
+        }
+    return render(request, 'source_delete.html', context)
+
+
+def source_details(request, pk):
+    source = get_object_or_404(Source, pk=pk)
+    context = {
+        'source': source
+    }
+    return render(request, 'source_details.html', context)
