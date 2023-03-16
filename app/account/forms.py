69efab390a26dd2ae2fd07938c2801cd36a9e7b7
vanya <vanya.ryabchenko@gmail.com>
2023-03-17 00:24:45 +0200

    hw13

---
+++ app/account/forms.py
@@ -0,0 +1,53 @@
+import uuid
+
+from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class UserSignUpForm(forms.ModelForm):
+
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password1',
+            'password2'
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password1'] != cleaned_data['password2']:
+            raise forms.ValidationError('Passwords should match!')
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password1']
+        user.set_password(password)
+        user.username = uuid.uuid4()
+        user.is_active = False
+        self._send_email()
+        user.save()
+        return user
+
+    def _send_email(self):
+        subject = 'Activate account'
+        path = reverse('account:activate', args=(self.instance.username,))
+        message = f'''
+                    {settings.HTTP_SCHEMA}://{settings.HOST}{path}
+                '''
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [self.instance.email],
+            fail_silently=False,
+        )
---
+++ app/account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from account.views import UserSignUpView, UserActivateView
+
+app_name = 'account'
+
+urlpatterns = [
+    path('signup/', UserSignUpView.as_view(), name='signup'),
+    path('activate/<uuid:username>/', UserActivateView.as_view(), name='activate')
+]
